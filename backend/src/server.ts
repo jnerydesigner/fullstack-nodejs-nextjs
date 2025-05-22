99155f5c98995b3994cfdd36fd92ce8c9d998cd4
jnerydesigner <jander.webmaster@gmail.com>
2025-05-22 09:42:34 -0400

    rEFLECT METATDATA

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
+import "dotenv/config";
+import "reflect-metadata";
 import { logger } from "@infra/logger/logger.log";
 import { uberRoutes } from "@presenters/routes/uber.route";
 import { viaCepRoutes } from "@presenters/routes/viacep.route";
-import "dotenv/config";
 import express, { Request, Response } from "express";
 
 const PORT = process.env.SERVER_PORT || 3333;
